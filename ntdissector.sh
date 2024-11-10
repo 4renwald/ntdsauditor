9e6d3ef0432054df8b3786df23d1fdc68a103014
4renwald <106856367+4renwald@users.noreply.github.com>
2024-11-10 14:09:04 -0500

    Update ntdissector.sh

--- ntdissector.sh
+++ ntdissector.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+outputdir="$1"
+path_hash="${outputdir#./}"
+
+set -e
+trap 'error_handling' EXIT
+error_handling() {
+  exit_status=$?
+  if [ "$exit_status" -ne 0 ]; then
+    print_error "There was an error while executing the script (Exit Status: $exit_status)"
+  fi
+}
+
+
 function print_success () {
     echo -e "[\x1B[01;32m+\x1B[0m] $1"
 }
@@ -17,35 +30,23 @@ function print_info () {
 }
 
 
-function help()
-{
-    echo "Syntax: ntdissector.sh [-ntds, -system, -outputdir]"
-    echo "options:"
-    echo "n      Location of NTDS.dit file."
-    echo "s   	Location of SYSTEM hive."
-    echo "o		Location of the output files."
-    echo
-}
+if [[ $# -eq 0 ]] ; then
+  print_error "No output directory supplied"
+  exit 2
+fi
 
-
-function check_arguments {
-	if [ -z "$ntds" ] || [ -z "$system" ] || [ -z "$outputdir" ]; then	print_error "ntds, system, and outputdir must be supplied."
-		help
-		exit 1
-	fi
-}
 
 function is_user_root () {
-	if [ "$EUID" -ne 0 ]; then
-		print_error "This script needs to be run using sudo"
-		exit 1
-	fi
+  if [ "$EUID" -ne 0 ]; then
+      print_error "This script needs to be run using sudo"
+      exit 1
+  fi
 }
 
 
 function change_directory_script () {
-	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-	cd $SCRIPT_DIR
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  cd $SCRIPT_DIR
 }
 
 
@@ -63,77 +64,52 @@ function dependencies () {
 
 
 function create_venv() {    
-	if [ -d ".venv" ]; then
-		print_warning "Virtual environment '.venv' already exists and will be used"
-	else
-		print_info "Creating virtualenv for ntdissector"
-		python3 -m venv ".venv"
-		print_success "virtual env created"
-	fi
+  if [ -d ".venv" ]; then
+      print_warning "Virtual environment '.venv' already exists and will be used"
+  else
+    print_info "Creating virtualenv for ntdissector"
+    python3 -m venv ".venv"
+    print_success "virtual env created"
+  fi
 }
 
 
 function install_ntdissector () {
-	source "./.venv/bin/activate"
-	if ! command -v ntdissector &> /dev/null
-	then
-		print_info "Installing ntdissector"
-		git clone --quiet https://github.com/synacktiv/ntdissector.git
-		yes | python3 -m pip install ntdissector/. --no-cache-dir -I \
-			-q -q -q --exists-action i
-	fi
-	deactivate
+  source "./.venv/bin/activate"
+  if ! command -v ntdissector &> /dev/null
+  then
+      print_info "Installing ntdissector"
+      git clone --quiet https://github.com/synacktiv/ntdissector.git
+      yes | python3 -m pip install ntdissector/. --no-cache-dir -I \
+          -q -q -q --exists-action i
+  fi
+  deactivate
 }
 
 
 function exec_ntdissector () {
-	print_info "Executing ntdissector on files provided in to_analyze folder"
-	source "./.venv/bin/activate"
-	ntdissector -ntds $ntds -system $system -outputdir $outputdir -ts -f all
-	print_success "JSON files stored in $outputdir/out"
-	print_info "Dumping NTLM hashes"
-	python3 $SCRIPT_DIR/ntdissector/ntdissector/tools/user_to_secretsdump.py $outputdir/out/*/*.json > $outputdir/out/ntlm-hashes
-	print_info "Parsing NTLM hashes in"
-	cat $outputdir/out/ntlm-hashes | cut -d : -f 4 |sort|uniq > $outputdir/out/ntlm-hashes-hashcat
-	print_success "NTLM hashes dumped in $outputdir/out/"
-	deactivate
+  print_info "Executing ntdissector on files provided in to_analyze folder"
+  source "./.venv/bin/activate"
+  local ntds_file=$(ls to_analyze | grep ".dit" | tail -n 1) 
+  local system_file=$(ls to_analyze | grep -E 'system|SYSTEM' | tail -n 1)
+  ntdissector -ntds to_analyze/$ntds_file -system to_analyze/$system_file -outputdir $outputdir -ts -f all
+  print_success "JSON files stored in $outputdir folder"
+  print_info "Dumping NTLM hashes"
+  python3 $SCRIPT_DIR/ntdissector/ntdissector/tools/user_to_secretsdump.py $SCRIPT_DIR/$path_hash/out/*/*.json > $SCRIPT_DIR/$path_hash/out/ntlm-hashes
+  print_info "Parsing NTLM hashes"
+  cat $SCRIPT_DIR/$path_hash/out/ntlm-hashes | cut -d : -f 4 |sort|uniq > $SCRIPT_DIR/$path_hash/out/ntlm-hashes-hashcat
+  print_success "NTLM hashes dumped in $outputdir/out/"
+  deactivate
 }
 
 
 function main () {
     is_user_root
-	check_arguments
     change_directory_script
     dependencies
     create_venv
     install_ntdissector
     exec_ntdissector
 }
-
-set -e
-trap 'error_handling' EXIT
-error_handling() {
-	exit_status=$?
-	if [ "$exit_status" -ne 0 ]; then
-    print_error "There was an error while executing the script (Exit Status: $exit_status)"
-	fi
-}
-
-while getopts ":hn:s:o:" option; do
-    case $option in
-		h) 
-			help
-			exit;;
-		n)
-			ntds=$OPTARG;;
-		s)
-			system=$OPTARG;;
-		o)
-			outputdir=$OPTARG;;
-		\?)
-			print_error "Invalid option"
-			exit;;
-	esac
-done
 
 main
